fe0f174a09437d4903ac7eed02d48210d8e70b28
Sudarshan <kumar.sudarshan6@gmail.com>
2024-01-30 13:26:37 +0530

    second

---
+++ App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Stock Average Calculator
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React at its best
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React at its best times 2
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
